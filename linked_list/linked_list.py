aa244ddd8b750a2e1aba07cc1f56d03239d7eaea
lizett <lizett503@gmail.com>
2022-03-20 15:48:13 -0700

    get_first and add_first functions work

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -9,14 +9,17 @@ class Node:
 # Defines the singly linked list
 class LinkedList:
     def __init__(self):
-      self.head = None # keep the head private. Not accessible outside this class
-
+        self.head = None # keep the head private. Not accessible outside this class
+        self.tail = None
     # returns the value in the first node
     # returns None if the list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def get_first(self):
-        pass
+        if self.head is None:
+            return None
+
+        return self.head.value
 
 
     # method to add a new node with the specific data value in the linked list
@@ -24,7 +27,12 @@ class LinkedList:
     # Time Complexity: ?
     # Space Complexity: ?
     def add_first(self, value):
-        pass
+        if self.head is None:
+            self.head = self.tail = Node(value)
+        else: 
+            new_node = Node(value,self.head)
+            self.head.previous = new_node 
+            self.head = new_node 
 
     # method to find if the linked list contains a node with specified value
     # returns true if found, false otherwise
