67216a6689af1f7fcc375bee358ee476449eacaa
Gabe Kelemen <gabe@gabekelemen.com>
2022-03-27 17:42:12 -0400

    Add advanced methods has cycle, middle value, nth from end

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,7 +1,9 @@
 
 # Defines a node in the singly linked list
-class Node:
+from distutils.command.build_scripts import first_line_re
 
+
+class Node:
     def __init__(self, value, next_node = None):
         self.value = value
         self.next = next_node
@@ -23,8 +25,8 @@ class LinkedList:
 
     # method to add a new node with the specific data value in the linked list
     # insert the new node at the beginning of the linked list
-    # Time Complexity:  
-    # Space Complexity:  
+    # Time Complexity:  O(1)
+    # Space Complexity:  O(1)
     def add_first(self, value):
         new_node = Node(value)
         new_node.next = self.head
@@ -34,8 +36,8 @@ class LinkedList:
 
     # method to find if the linked list contains a node with specified value
     # returns true if found, false otherwise
-    # Time Complexity:  
-    # Space Complexity:  
+    # Time Complexity:  O(N)
+    # Space Complexity:  O(N)
     def search(self, value):
         current = self.head
         while current != None:
@@ -45,8 +47,8 @@ class LinkedList:
         return False
 
     # method that returns the length of the singly linked list
-    # Time Complexity: O(n)
-    # Space Complexity: 
+    # Time Complexity: O(N)
+    # Space Complexity: O(N)
     def length(self):
         current = self.head  #initialize current 
         counter = 0
@@ -59,8 +61,8 @@ class LinkedList:
     # method that returns the value at a given index in the linked list
     # index count starts at 0
     # returns None if there are fewer nodes in the linked list than the index value
-    # Time Complexity:  
-    # Space Complexity:  
+    # Time Complexity:  O(N)
+    # Space Complexity:  O(N)
     def get_at_index(self, index):
         if index < 0:
             return None
@@ -75,8 +77,8 @@ class LinkedList:
 
     # method that returns the value of the last node in the linked list
     # returns None if the linked list is empty
-    # Time Complexity:  
-    # Space Complexity:  
+    # Time Complexity:  O(N)
+    # Space Complexity:  O(N)
     def get_last(self):
         if self.head == None:
             return None
@@ -86,8 +88,8 @@ class LinkedList:
         return current.value
 
     # method that inserts a given value as a new last node in the linked list
-    # Time Complexity:  
-    # Space Complexity:  
+    # Time Complexity:  O(N)
+    # Space Complexity:  O(N)
     def add_last(self, value):
         new_node = Node(value)
         if self.head == None:
@@ -111,9 +113,9 @@ class LinkedList:
                 current = current.next
             return max_value
 
-    # method to delete the first node found with specified value
-    # Time Complexity: O(n)
-    # Space Complexity: O(1)
+    # method to delete the first node found with "specified value"
+    # Time Complexity: O(N)
+    # Space Complexity: O(N) like a search bcs specified value  
     def delete(self, value):
         if self.head == None:
             return None
@@ -130,8 +132,8 @@ class LinkedList:
         return current
 
     # method to print all the values in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(N)
+    # Space Complexity: O(N)
     def visit(self):
         helper_list = []
         current = self.head
@@ -144,8 +146,8 @@ class LinkedList:
 
     # method to reverse the singly linked list
     # note: the nodes should be moved and not just the values in the nodes
-    # Time Complexity: O(n)
-    # Space Complexity: O(1)
+    # Time Complexity: O(N)
+    # Space Complexity: O(N)
     def reverse(self):
         #initializes 
         previous = None
@@ -165,32 +167,58 @@ class LinkedList:
   
     ## Advanced/ Exercises
     # returns the value at the middle element in the singly linked list
-    # Time Complexity:  
-    # Space Complexity:  
+    # Time Complexity:  O(N)
+    # Space Complexity:  O(N)
     def find_middle_value(self):
-        current = self.head
-        current2 = self.head
+    # slow moves 1 step, fast moves 2, so when fast reaches the end the slow would be half way to the end
+        slow_pointer = self.head
+        fast_pointer = self.head
+        while fast_pointer != None and fast_pointer.next != None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
         
-        while current and current2:
-            current = current.next
-            current2 = current.next.next
-        
-        return current
+        return slow_pointer.value
 
     # find the nth node from the end and return its value
     # assume indexing starts at 0 while counting to n
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(N)
+    # Space Complexity: O(N)
     def find_nth_from_end(self, n):
-        pass
+         
+        if self.head == None :
+            return None 
+        temp_index = self.length() - n -1    
+        if temp_index < 0:
+            return None  
+        else:
+            return self.get_at_index(temp_index)
+        
 
     # checks if the linked list has a cycle. A cycle exists if any node in the
     # linked list links to a node already visited.
     # returns true if a cycle is found, false otherwise.
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(N)
+    # Space Complexity: O(N)
     def has_cycle(self):
-        pass
+        current_node = self.head
+        set_of_nodes_visited = set()
+        while current_node != None:
+            if current_node in set_of_nodes_visited:
+                return True
+            set_of_nodes_visited.add(current_node)
+            current_node = current_node.next
+        return False
+    
+    
+        # slow_pointer = self.head
+        # fast_pointer = self.head
+        
+        # while fast_pointer != None and fast_pointer.next != None:
+        #     slow_pointer = slow_pointer.next
+        #     fast_pointer = fast_pointer.next.next
+        #     if slow_pointer == fast_pointer:
+        #         return True
+        # return False
 
     # Helper method for tests
     # Creates a cycle in the linked list for testing purposes
