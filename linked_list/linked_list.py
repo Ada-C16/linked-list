f95f86a640e665f06f6b023478742311ddcb4da5
Kbhlee2121 <Kbhlee2121@gmail.com>
2022-04-03 19:26:05 -0700

    Solve 6 out of 10 functions

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -10,13 +10,15 @@ class Node:
 class LinkedList:
     def __init__(self):
       self.head = None # keep the head private. Not accessible outside this class
-
+      self.tail = None
     # returns the value in the first node
     # returns None if the list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def get_first(self):
-        pass
+        if self.head is None:
+            return None
+        return self.head.value
 
 
     # method to add a new node with the specific data value in the linked list
@@ -24,20 +26,39 @@ class LinkedList:
     # Time Complexity: ?
     # Space Complexity: ?
     def add_first(self, value):
-        pass
+        # if self.head is None:
+        #     self.head = self.tail = None(value)
+        # else:
+            # self.head set to be next_node
+            new_node = Node(value, self.head)
+            # self.head.previous = new_node
+            new_node.next = self.head
+            self.head = new_node
 
     # method to find if the linked list contains a node with specified value
     # returns true if found, false otherwise
     # Time Complexity: ?
     # Space Complexity: ?
     def search(self, value):
-        pass
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
 
     # method that returns the length of the singly linked list
     # Time Complexity: ?
     # Space Complexity: ?
     def length(self):
-        pass
+        count = 0
+        current = self.head
+
+        while current:
+            count += 1
+            current = current.next
+
+        return count
 
     # method that returns the value at a given index in the linked list
     # index count starts at 0
@@ -45,20 +66,52 @@ class LinkedList:
     # Time Complexity: ?
     # Space Complexity: ?
     def get_at_index(self, index):
-        pass
+        if self.length() < index:
+            return None
+        
+        current = self.head
+        current_index = 0
+
+        while current:
+            if current_index == index:
+                return current.value
+            current = current.next
+            current_index += 1 
+        return None
+        
 
     # method that returns the value of the last node in the linked list
     # returns None if the linked list is empty
     # Time Complexity: ?
     # Space Complexity: ?
     def get_last(self):
-        pass
+        if self.length() == 0:
+            return None
+
+        current = self.head
+
+        while current.next:
+            current = current.next
+        return current.value
 
     # method that inserts a given value as a new last node in the linked list
     # Time Complexity: ?
     # Space Complexity: ?
     def add_last(self, value):
-        pass
+        new_node = Node(value, self.head)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+        
+
+        # new_node.previous = self.tail
+        # self.tail = new_node
 
     # method to return the max value in the linked list
     # returns the data value and not the node
@@ -86,10 +139,27 @@ class LinkedList:
 
     # method to reverse the singly linked list
     # note: the nodes should be moved and not just the values in the nodes
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def reverse(self):
-        pass
+        # if head and tails are equal to each other and/or None
+        if self.head == self.tail:
+            return
+        
+        current = self.head
+        
+        # each iteration, current is changing to next node in chain
+        while current is not None:
+            prev = current.previous
+            next = current.next
+            current.next = prev
+            current.previous = next
+            current = next
+
+        #swap head and tail
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
   
     ## Advanced/ Exercises
     # returns the value at the middle element in the singly linked list
