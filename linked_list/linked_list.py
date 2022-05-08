23926394c7c580ae825d9716fee8a1b81f23cbd8
ngoziamaefule <ngoziamaefule1@gmail.com>
2022-05-07 21:26:59 -0400

    Finished necessary linked list functions

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -9,71 +9,148 @@ class Node:
 # Defines the singly linked list
 class LinkedList:
     def __init__(self):
-      self.head = None # keep the head private. Not accessible outside this class
+        self.head = None # keep the head private. Not accessible outside this class
 
     # returns the value in the first node
     # returns None if the list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def get_first(self):
-        pass
+        if not self.head:
+            return None
+        
+        return self.head.value
 
 
     # method to add a new node with the specific data value in the linked list
     # insert the new node at the beginning of the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def add_first(self, value):
-        pass
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
 
     # method to find if the linked list contains a node with specified value
     # returns true if found, false otherwise
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def search(self, value):
-        pass
+        if not self.get_first():
+            return False
+        
+        current_node = self.head
+
+        while current_node != None:
+            if current_node.value == value:
+                return True
+            else:
+                current_node = current_node.next
+        
+        return False
 
     # method that returns the length of the singly linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def length(self):
-        pass
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def length(self):        
+        counter = 0
+        current_node = self.head
+
+        while current_node != None:
+            counter += 1
+            current_node = current_node.next
+        
+        return counter
 
     # method that returns the value at a given index in the linked list
     # index count starts at 0
     # returns None if there are fewer nodes in the linked list than the index value
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def get_at_index(self, index):
-        pass
+        if self.length() < index:
+            return None
+        
+        current_node = self.head
+        count = 0
+        while count < index:
+            current_node = current_node.next
+            count += 1
+
+        return current_node.value
+
 
     # method that returns the value of the last node in the linked list
     # returns None if the linked list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def get_last(self):
-        pass
+        current_node = self.head
+
+        if current_node == None:
+            return None
+
+        while current_node.next != None:
+            current_node = current_node.next
+        
+        return current_node.value
 
     # method that inserts a given value as a new last node in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def add_last(self, value):
-        pass
+        if self.head == None:
+            self.head = Node(value)
+            return self
+        
+        current_node = self.head
+        while current_node.next != None:
+            current_node = current_node.next
+
+        new_node = Node(value)
+        current_node.next = new_node
+        return self
 
     # method to return the max value in the linked list
     # returns the data value and not the node
     def find_max(self):
-        pass
+        if self.head == None:
+            return self.head
+        
+        current_node = self.head
+        max_node = current_node
+
+        while current_node != None:
+            if current_node.value > max_node.value:
+                max_node = current_node
+                current_node = current_node.next
+            else:
+                current_node = current_node.next
+        
+        return max_node.value
 
     # method to delete the first node found with specified value
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def delete(self, value):
-        pass
+        current_node = self.head
+        if current_node == None:
+            return None
+
+        if self.head.value == value:
+            self.head = self.head.next
+            return
+
+        while current_node:
+            if current_node.next.value != value:
+                current_node = current_node.next
+            else:
+                current_node.next = current_node.next.next
+                return
 
     # method to print all the values in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def visit(self):
         helper_list = []
         current = self.head
@@ -86,11 +163,22 @@ class LinkedList:
 
     # method to reverse the singly linked list
     # note: the nodes should be moved and not just the values in the nodes
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def reverse(self):
-        pass
-  
+        previous = None
+        current = self.head
+        next_node = None
+
+        while current is not None:
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+        self.head = previous
+
+        return 
+
     ## Advanced/ Exercises
     # returns the value at the middle element in the singly linked list
     # Time Complexity: ?
