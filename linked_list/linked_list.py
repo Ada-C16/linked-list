789bee6b688d18858f17319601949a0c2cfb3331
sjolivas <sjolivas@gmail.com>
2022-07-17 15:52:05 -0600

    added to get_last and find_max, passed tests

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,9 +1,6 @@
 
 # Defines a node in the singly linked list
 
-from ctypes import pointer
-
-
 class Node:
 
     def __init__(self, value, next_node = None):
@@ -90,8 +87,8 @@ class LinkedList:
 
     # method that returns the value of the last node in the linked list
     # returns None if the linked list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def get_last(self):
         # pointer = self.head
         # next_node = pointer.next
@@ -111,15 +108,38 @@ class LinkedList:
         return pointer.value
 
     # method that inserts a given value as a new last node in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def add_last(self, value):
-        pass
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last = self.head
+        while (last.next):
+            last = last.next
+
+        last.next = new_node
+        
 
     # method to return the max value in the linked list
     # returns the data value and not the node
     def find_max(self):
-        pass
+        if self.head is None:
+            return None
+
+        pointer = self.head
+        maximum = self.head.value
+
+        while (pointer is not None):
+            if (maximum < pointer.value):
+                maximum = pointer.value
+            pointer = pointer.next
+        
+        return maximum
+
 
     # method to delete the first node found with specified value
     # Time Complexity: ?
